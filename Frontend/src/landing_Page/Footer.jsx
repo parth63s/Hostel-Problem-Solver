22b71556e02304334b9b1ba43e8c3b0989025545
Parth Ramani <parthramani63@gmail.com>
2024-11-21 13:59:34 +0530

    Design Navbar and Footer

--- Frontend/src/landing_Page/Footer.jsx
+++ Frontend/src/landing_Page/Footer.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    return ( 
-        <h1>Footer</h1>
-     );
+  return (
+    <footer className="mt-5 border-top min-background pt-3 bg-white fixed-bottom" >
+      <div class="f-info">
+        <div class="f-info-socials ">
+          <i class="fa-regular fa-copyright"></i>
+          <i class="fa-brands fa-instagram"></i>
+        </div>
+        <div class="f-info-brand pt-2">Hostel Wardens Limited</div>
+        <div class="f-info-links pt-2 pb-3">
+          <a href="/help" class>
+            Help
+          </a>
+          <a href="/privacy">Privacy</a>
+          <a href="/terms">Terms</a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
