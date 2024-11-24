c4227954df7fb4fe925f8cb8a08d284be25e529e
Parth Ramani <parthramani63@gmail.com>
2024-11-24 21:53:06 +0530

    Design-Worker-Page

--- Frontend/src/landing_Page/Worker/Problems.jsx
+++ Frontend/src/landing_Page/Worker/Problems.jsx
@@ -2,7 +2,62 @@ import React from 'react';
 
 function Problems() {
     return ( 
-        <h1>Problems</h1>
+        <div className="container m-5 p-2 min-background" style={{backgroundColor:"white"}}>
+            <h1 className='text-center fs-3 mb-4'>All Problem</h1>
+            <table className='table double-border table-bordered text-center'>
+                <tr>
+                    <th>No.</th>
+                    <th>Name</th>
+                    <th>Hostel Name</th>
+                    <th>Room no.</th>
+                    <th>Problem</th>
+                    <th>Show</th>
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>parth</td>
+                    <td>milkha</td>
+                    <td>318</td>
+                    <td>shoavar</td>
+                    <td><button className='btn'>show</button></td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>parth</td>
+                    <td>milkha</td>
+                    <td>318</td>
+                    <td>shoavar</td>
+                    <td><button className='btn'>show</button></td>
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>parth</td>
+                    <td>milkha</td>
+                    <td>318</td>
+                    <td>shoavar</td>
+                    <td><button className='btn'>show</button></td>
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>parth</td>
+                    <td>milkha</td>
+                    <td>318</td>
+                    <td>shoavar</td>
+                    <td><button className='btn'>show</button></td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>parth</td>
+                    <td>milkha</td>
+                    <td>318</td>
+                    <td>shoavar</td>
+                    <td><button className='btn'>show</button></td>
+                </tr>
+            </table>
+        </div>
      );
 }
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import './index.css'
 import HomePage from './landing_Page/Home/HomePage'
 import StudentPage from './landing_Page/Student/StudentPage'
+import WorkerPage from './landing_Page/Worker/WorkerPage'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <StudentPage/>
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/workerpanel" element={<WorkerPage/>}> </Route>
+    </Routes>
+  </BrowserRouter>
 )
